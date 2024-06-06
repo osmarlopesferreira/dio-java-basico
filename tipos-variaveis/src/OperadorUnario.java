8a9520bc4bce82e14691caa466cf4f86a56bcc2c
Osmar <osmarlf1@gmail.com>
2024-06-06 12:01:24 -0300

    criando um sistema para smart tv

---
+++ tipos-variaveis/src/OperadorUnario.java
@@ -0,0 +1,9 @@
+public class OperadorUnario {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        System.out.println(- numero);
+
+        System.out.println(numero);
+    }
+}
